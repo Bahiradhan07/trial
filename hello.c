5284657c0a0b2dadc7ef45acc2b61e18d6c8c1cc
Bahiradhan07 <bahiradhan.t@pixelexpert.net>
2025-08-22 18:10:13 +0530

    Test deployment

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello! GitHub Actions deployed me ahh!\n");
+    printf("Hello! GitHub Actions deployed me ahhh!\n");
     while(1) {} // keep running
     return 0;
 }
